bf08039fa4fd26b63087a7af2e5ffa3250f44fe2
Vaibhav <itsvaibhav002@gmail.com>
2024-07-11 18:58:30 +0530

    input

---
+++ DAY1/input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String input = sc.next(); //next use only for one word.
+        String input1 = sc.nextLine(); //nextline use for all string including space
+        System.out.println(input);
+        System.out.println(input1);
+    }
+}
