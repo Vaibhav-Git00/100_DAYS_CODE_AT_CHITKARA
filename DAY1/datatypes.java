22f6990c007d6f58726208366071e8d03ea23861
Vaibhav <itsvaibhav002@gmail.com>
2024-07-11 18:43:47 +0530

    add file

---
+++ DAY1/datatypes.java
@@ -0,0 +1,19 @@
+public class datatypes {
+    public static void main(String[] args) {
+        byte a=10;
+        System.out.println(a);
+        float b = 10.3f;   
+        System.out.println(b);
+        char ch= 'a';
+        System.out.println(ch);
+        int d=10;
+        System.out.println(d);
+        //double 
+        //long
+        boolean var = true;
+        System.out.println(var);
+        short n= 340;
+        System.out.println(n);
+
+    }
+}
---
+++ DAY1/variables.java
@@ -0,0 +1,9 @@
+public class variables {
+    public static void main(String[] args) {
+        int a=10;
+        int b=90;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+//a and b are the variables.
