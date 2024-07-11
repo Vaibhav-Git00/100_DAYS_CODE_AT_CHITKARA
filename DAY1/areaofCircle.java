b3e227df167e8bea017626d888bb177ba41bd2cf
Vaibhav <itsvaibhav002@gmail.com>
2024-07-11 21:33:43 +0530

    update

---
+++ DAY1/areaofCircle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class areaofCircle {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        Float pie= 3.14f;
+        int radius= sc.nextInt();
+        Float Area= pie*radius*radius;
+        System.out.println(Area);
+    }
+}
--- DAY1/input.java
+++ DAY1/input.java
@@ -3,8 +3,15 @@ public class input {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         String input = sc.next(); //next use only for one word.
-        String input1 = sc.nextLine(); //nextline use for all string including space
         System.out.println(input);
+        
+        String input1 = sc.nextLine(); //nextline use for all string including space
         System.out.println(input1);
+
+        int input2 = sc.nextInt();
+        System.out.println(input2);
+
+        Float input3 = sc.nextFloat();
+        System.out.println(input3);
     }
 }
