5cb7085f53dc52b58963f1b98e7b1061b416d683
Vaibhav <itsvaibhav002@gmail.com>
2024-07-12 09:57:28 +0530

    update

---
+++ Assignments1/A1.java
@@ -0,0 +1,10 @@
+
+//passOrfail_ternary operator
+
+public class A1 {
+    public static void main(String[] args) {
+        int marks=45;
+        String result= (marks>40)? "pass": "fail";
+        System.out.println(result);
+    }
+}
