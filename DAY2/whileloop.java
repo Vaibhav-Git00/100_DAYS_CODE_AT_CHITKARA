55c81f5e57d873d0379211d78f19ee425d005af0
Vaibhav <itsvaibhav002@gmail.com>
2024-07-12 12:22:27 +0530

    update

--- DAY2/whileloop.java
+++ DAY2/whileloop.java
@@ -1,5 +1,14 @@
 
-
+import java.util.*;
 public class whileloop {
-    
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+      
+        int n=1;
+        while (n<=10) {
+            System.out.println(n);
+            n++;
+        } 
+        sc.close();
+    }
 }
