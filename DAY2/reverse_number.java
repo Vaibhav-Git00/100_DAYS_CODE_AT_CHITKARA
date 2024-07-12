42489722b3e0bd9e50a0079495cbf912fe6f8a55
Vaibhav <itsvaibhav002@gmail.com>
2024-07-12 13:14:30 +0530

    update

--- DAY2/reverse_number.java
+++ DAY2/reverse_number.java
@@ -1,15 +1,32 @@
 
+// import java.util.*;
+// public class reverse_number {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n= sc.nextInt();
+//         while(n>0){
+//             int lastdigit = n%10;
+//             System.out.print(lastdigit + " ");
+//             n=n/10;
+//         }
+//         System.out.println();
+//         sc.close();
+//     }
+// }
+
+
 import java.util.*;
 public class reverse_number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
+        int rev=0;
         while(n>0){
             int lastdigit = n%10;
-            System.out.print(lastdigit + " ");
+            rev= (rev*10) + lastdigit;
             n=n/10;
         }
-        System.out.println();
+        System.out.println(rev);
         sc.close();
     }
 }
