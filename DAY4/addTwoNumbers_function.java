d2d76703d38c60e5d58ca121d07d7ffaa341d472
Vaibhav <itsvaibhav002@gmail.com>
2024-07-14 19:14:30 +0530

    updates

---
+++ DAY4/addTwoNumbers_function.java
@@ -0,0 +1,38 @@
+//void type 
+
+// import java.util.*;
+// public class addTwoNumbers_function {
+//     public static void sum(int a ,int b){
+//         int result =a+b;
+//         System.out.println("the sum is ="+ result);
+
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int num1=sc.nextInt();
+//         int num2=sc.nextInt();
+//         sum(num1,num2);
+//         sc.close();
+//     }
+// }
+
+
+
+//return type 
+
+import java.util.*;
+public class addTwoNumbers_function {
+    public static int sum(int a,int b){
+        int sum = a+b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();   
+        int b=sc.nextInt();
+        int sum=sum(a,b);
+        System.out.println("sum is = "+ sum);   
+    }
+}
---
+++ DAY4/helloworld_functions.java
@@ -0,0 +1,8 @@
+public class helloworld_functions{
+    public static void printhelloworld() {
+        System.out.println("hello world");
+    }
+    public static void main(String[] args) {
+        printhelloworld();
+    }
+}
