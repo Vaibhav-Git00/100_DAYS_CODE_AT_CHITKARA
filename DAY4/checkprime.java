a0b5a00546842acc1ddfc05e47f600d3521e1dd3
Vaibhav <itsvaibhav002@gmail.com>
2024-07-15 13:08:58 +0530

    updates

--- DAY4/checkprime.java
+++ DAY4/checkprime.java
@@ -1,11 +1,45 @@
+// import java.util.Scanner;
+
+// public class checkprime {
+//     public static void is_prime(int a){
+//         int isprime=1;
+//         for(int i=2;i<a-1;i++){
+//             if(a%i==0){
+//                 isprime=0;
+//             }
+//         }
+//         if(isprime==1){
+//             System.out.println("this is prime");
+//         }
+//         else{
+//             System.out.println("this is not a prime");
+//         }
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int num=sc.nextInt();
+//         is_prime(num);
+//         sc.close();
+//     }
+// }
+
+
+
+//optmized method
 import java.util.Scanner;
 
 public class checkprime {
     public static void is_prime(int a){
+        if(a==2){
+            System.out.println("this is prime");
+            return;
+        }
+        
         int isprime=1;
-        for(int i=2;i<a-1;i++){
+        for(int i=2;i<=Math.sqrt(a);i++){
             if(a%i==0){
                 isprime=0;
+                break;
             }
         }
         if(isprime==1){
@@ -15,6 +49,7 @@ public class checkprime {
             System.out.println("this is not a prime");
         }
     }
+    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int num=sc.nextInt();
