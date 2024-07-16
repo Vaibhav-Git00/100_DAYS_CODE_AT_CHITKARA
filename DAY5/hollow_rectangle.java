7e8f1102bfc0d35b651abf861f98e28ee6727259
Vaibhav <itsvaibhav002@gmail.com>
2024-07-16 19:44:59 +0530

    update

--- DAY5/hollow_rectangle.java
+++ DAY5/hollow_rectangle.java
@@ -2,10 +2,8 @@ import java.util.*;
 public class hollow_rectangle {
 
     public static void main(String[] args) {
-
-
-
-
+        Scanner sc=new Scanner(System.in);
+        
         sc.close();
         
     }
