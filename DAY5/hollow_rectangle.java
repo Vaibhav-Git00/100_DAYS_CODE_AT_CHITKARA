35246366c89c64f57ea29e97908b8f910662b5dd
Vaibhav <itsvaibhav002@gmail.com>
2024-07-16 22:55:18 +0530

    update

--- DAY5/hollow_rectangle.java
+++ DAY5/hollow_rectangle.java
@@ -1,9 +1,23 @@
 import java.util.*;
 public class hollow_rectangle {
+    public static void pattern(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||i==n-1||j==0||j==n-1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        
+        int num=sc.nextInt();
+        pattern(num);
         sc.close();
         
     }
