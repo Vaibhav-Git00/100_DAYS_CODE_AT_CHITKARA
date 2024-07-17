c7a913f83e58c911733027881bb8b2dbace48a6c
Vaibhav <itsvaibhav002@gmail.com>
2024-07-17 15:35:53 +0530

    update

--- DAY6/Butterfly_Pattern.java
+++ DAY6/Butterfly_Pattern.java
@@ -1,5 +1,12 @@
-
-
+import java.util.*;
 public class Butterfly_Pattern {
-    
+    public static void pattern(int n){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+
 }
