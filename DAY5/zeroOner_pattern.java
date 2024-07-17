2a5420b444d5a42d83e7ab25466a580d0f00e831
Vaibhav <itsvaibhav002@gmail.com>
2024-07-17 15:18:27 +0530

    update

--- DAY5/zeroOner_pattern.java
+++ DAY5/zeroOner_pattern.java
@@ -1,17 +1,20 @@
 // 1 
-// 2 3 
-// 4 5 6 
-// 7 8 9 10 
-// 11 12 13 14 15 
+// 0 1 
+// 1 0 1 
+// 0 1 0 1 
+// 1 0 1 0 1 
 
-import java.util.*;
-public class floyds_pattern {
+import java.util.Scanner;
+public class zeroOner_pattern {
     public static void pattern(int n){
-        int counter=1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(counter+" ");
-                counter++;
+                if((i+j)%2==0){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
             }
             System.out.println();
         }
@@ -20,7 +23,6 @@ public class floyds_pattern {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         pattern(n);
-        sc.close();
-        
+        sc.close(); 
     }
 }
