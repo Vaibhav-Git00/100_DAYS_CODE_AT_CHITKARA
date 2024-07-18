fc1fdbeda2b088a68625f596b948dbf6219bd9c9
Vaibhav <itsvaibhav002@gmail.com>
2024-07-18 11:16:39 +0530

    update

--- DAY6/Butterfly_Pattern.java
+++ DAY6/Butterfly_Pattern.java
@@ -1,12 +1,47 @@
+// *             * 
+// * *         * * 
+// * * *     * * * 
+// * * * * * * * * 
+// * * * * * * * * 
+// * * *     * * * 
+// * *         * * 
+// *             * 
+
+
+
 import java.util.*;
 public class Butterfly_Pattern {
     public static void pattern(int n){
-        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern(n);
+        sc.close();
     }
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
 
 }
