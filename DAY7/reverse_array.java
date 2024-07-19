1d1ba1d03cd480b21d0ba1a95af4bb8c4d3aac75
Vaibhav <itsvaibhav002@gmail.com>
2024-07-19 17:07:25 +0530

    update

--- DAY7/reverse_array.java
+++ DAY7/reverse_array.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class reverse_array {
     public static int reverse(int arr[]){
         int start=0,end=arr.length-1;
@@ -5,18 +7,26 @@ public class reverse_array {
             int temp=arr[end];
             arr[end]=arr[start];
             arr[start]=temp;
-
             start++;
             end--;
+            
         }
         return -1;
     }
     public static void main(String[] args) {
-        int num[]={45,78,43,24,78,8};
+        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int num[]=new int[n];
+        for(int i=0;i<num.length;i++){
+            num[i]=sc.nextInt();
+        }
+
         reverse(num);
         for(int i=0;i<num.length;i++){
             System.out.print(num[i]+ " ");
         }
         System.out.println();
+        sc.close();
     }
 }
