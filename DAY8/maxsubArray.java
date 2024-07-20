638873f0b46dd6b2aea70547524cfe4f2cc838d3
Vaibhav <itsvaibhav002@gmail.com>
2024-07-20 17:57:13 +0530

    update

---
+++ DAY8/maxsubArray.java
@@ -0,0 +1,26 @@
+public class maxsubArray {
+    public static void sub(int arr[]){
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            int start=i;
+            for(int j=i;j<arr.length;j++){
+                int end =j;
+                sum=0;
+                for(int k=start;k<=end;k++){
+                    sum+=arr[k];
+                }
+                System.out.println(sum);
+                if(max<sum){
+                    max=sum;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println(max);
+    }
+    public static void main(String[] args) {
+        int n[]={1,2,3,4,5};
+        sub(n);
+    }
+}
