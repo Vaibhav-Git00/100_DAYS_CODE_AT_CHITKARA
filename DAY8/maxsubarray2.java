e5ec9e26019432314336af460551c112c80efd4f
Vaibhav <itsvaibhav002@gmail.com>
2024-07-20 20:25:27 +0530

    update

---
+++ DAY8/maxsubarray2.java
@@ -0,0 +1,29 @@
+//prefix sum optimise method
+
+public class maxsubarray2 {
+    public static void maxsub(int arr[]){
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        int prefix[]= new int[arr.length];
+        prefix[0]=arr[0];
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+arr[i];
+        }
+        for(int i=0;i<arr.length;i++){
+            int start=i;
+            for(int j=i;j<arr.length;j++){
+                int end=j;
+                sum= start == 0 ? prefix[end]: prefix[end]- prefix[start-1];
+                if(max<sum){
+                    max=sum;
+                }
+            }
+        }
+        System.out.println("the sub max is:"+max);
+    }
+    public static void main(String[] args) {
+        int n[]={1,2,3,4,55,6};
+        maxsub(n);
+        
+    }
+}
