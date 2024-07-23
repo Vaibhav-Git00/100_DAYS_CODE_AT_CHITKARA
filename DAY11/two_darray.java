47ef0a28bd267b2923770d0481c7adcb883a041e
Vaibhav <itsvaibhav002@gmail.com>
2024-07-23 21:57:25 +0530

    update

--- DAY11/two_darray.java
+++ DAY11/two_darray.java
@@ -1,9 +1,34 @@
-
+import java.util.*;
 public class two_darray {
-    public static void array(int arr[]){
-        
+    public static boolean search(int arr[][],int key){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                if(arr[i][j]==key){
+                    System.out.println(i +","+ j);
+                    return true;
+                }
+            }
+        }
+        System.out.println("not found");
+        return false;
     }
     public static void main(String[] args) {
-        
+        int arr[][]=new int[3][3];
+        int n=arr.length,m=arr[0].length;
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        search(arr, 5);
+        sc.close();
     }
 }
