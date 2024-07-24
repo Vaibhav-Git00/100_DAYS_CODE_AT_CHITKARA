5c667dda8245bc0e1255c0c80d80e1b68b4fc071
Vaibhav <itsvaibhav002@gmail.com>
2024-07-24 22:30:59 +0530

    update

---
+++ DAY13/string.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class string {
+    public static void main(String[] args) {
+        // Scanner sc=new Scanner(System.in);     // string are immutable
+        // String name=sc.nextLine();
+        // String kaa=sc.next();
+        // System.out.println(name);
+        // System.out.println(kaa);
+        // sc.close();
+        String name="vaibjav kdjoejjs";
+        System.out.println(name.length()); ////string ki length nikalne la function
+
+
+        String firstletter="vaibhav";
+        String secondletter="kesharwani";
+        String combine=firstletter + " " + secondletter;    //concanating of two string with the help of '+'.
+        System.out.println(combine);
+
+        
+    }
+   
+}
