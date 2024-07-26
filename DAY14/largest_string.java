8dcecbfd8404055d2c4dcc5cb2e00aa2da1750a5
Vaibhav <itsvaibhav002@gmail.com>
2024-07-26 11:50:26 +0530

    update

---
+++ DAY14/largest_string.java
@@ -0,0 +1,13 @@
+public class largest_string {
+
+    public static void main(String[] args) {
+        String fruits[]={"mango","banana","cherry"};
+        String largest=fruits[0];
+        for(int i=1;i<fruits.length;i++){
+            if(largest.compareTo(fruits[i])<0){        
+                largest=fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
