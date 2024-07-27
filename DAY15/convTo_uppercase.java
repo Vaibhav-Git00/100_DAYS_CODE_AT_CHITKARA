287ce749e76ecd057a3af2f01bc3264cc2ebd41b
Vaibhav <itsvaibhav002@gmail.com>
2024-07-27 16:37:47 +0530

    update

--- DAY15/convTo_uppercase.java
+++ DAY15/convTo_uppercase.java
@@ -1,6 +1,26 @@
 public class convTo_uppercase {
+
+    public static String uppercase(String str){
+        StringBuilder sb=new StringBuilder();
+
+        char ch=Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' ' && i<str.length()-1){     //2nd case is liye laga hai ,ki input ka last wala empty na utha lee , i mean empty space ke baad koi character hona chahiye//
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+    
     public static void main(String[] args) {
-         
+        String strr="my name is vaibhav kesharwani ";
+        System.out.println(uppercase(strr));
     }
 }
  
