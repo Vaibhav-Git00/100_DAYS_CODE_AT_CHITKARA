43483d79ce0c98da8c7b7c7ea475d837d094c6ca
Vaibhav <itsvaibhav002@gmail.com>
2024-07-30 17:51:35 +0530

    update

--- DAY18/oops_concept.java
+++ DAY18/oops_concept.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
 
 public class oops_concept {
     public static void main(String[] args) {
-        
+        String s = "1 box has 3 blue 4 red 6 green and 12 yellow marbles";
+        String num = s.replaceAll("[a-zA-Z]", " ").replaceAll("\\s+", " ").trim(); 
+        String nu = s.replaceAll("[a-zA-Z]", " ").trim(); 
+        String[] str = num.split(" ");
+        System.out.println(nu);
+        System.out.println(num);
+        System.out.println(Arrays.toString(str));
     }
 }
