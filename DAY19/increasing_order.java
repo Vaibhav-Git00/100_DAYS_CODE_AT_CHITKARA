cfcdaa74228d8a0870d4f7e75984e2577db833b5
Vaibhav <itsvaibhav002@gmail.com>
2024-07-30 21:01:12 +0530

    update

--- DAY19/increasing_order.java
+++ DAY19/increasing_order.java
@@ -8,8 +8,17 @@ public class increasing_order {
         System.out.print(n+" ");
         
     }
+
+    public static void incr(int a,int n){
+        if(a>n) return;
+        System.out.print(a+" ");
+        incr(a+1, n);
+        return;
+    }
     public static void main(String[] args) {
         int n=10;
         increasing(n);
+        System.out.println();
+        incr(1,n);
     }
 }
