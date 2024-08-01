b898420c2028141fc2ece5838ba02d0fe8543085
Vaibhav <itsvaibhav002@gmail.com>
2024-08-01 14:22:55 +0530

    update

--- DAY20/natural_number.java
+++ DAY20/natural_number.java
@@ -1,3 +1,14 @@
 public class natural_number {
-    
+    public static int natural(int n){
+        if(n==1){
+            return 1;
+        }
+        int ans=natural(n-1);
+        int result=n+ans;
+        return result;
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(natural(n));
+    }
 }
