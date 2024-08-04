a76252eff947c4b0c4c80d599f6a74a5042f4472
Vaibhav <itsvaibhav002@gmail.com>
2024-08-04 14:47:51 +0530

    update

--- DAY22/friends_pairing.java
+++ DAY22/friends_pairing.java
@@ -3,9 +3,13 @@ public class friends_pairing {
         if(n==1||n==2){
             return n;
         }
+        //single
         int c1=pairing(n-1);
-        int c2= pairing(n-2);
-        int pairways= (n-1) * c2;
+        //pair
+        int c2= pairing(n-2);        
+        int pairways= (n-1) * c2;     //n-1 is kitne pairs ban satkte haii and multilpy be c2 is the showing of pair choices 
+
+        //total ways
         int total= c1+ pairways;
         return total;
     }
