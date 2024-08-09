eca97e4184fd664320e7712a45929b3dd34a8ed2
Vaibhav <itsvaibhav002@gmail.com>
2024-08-09 23:35:34 +0530

    update

--- DAY26/length.java
+++ DAY26/length.java
@@ -19,36 +19,50 @@
 
 
 
+// public class length {
+//     public static int count(String str){
+//         if(str.isEmpty()) return 0;
+//         return 1+count(str.substring(1));
+//     }
+
+//     public static void main(String[] args) {
+//         String input="hello";
+//         int length=count(input);
+//         System.out.println(length);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class length {
-    public static int count(String str){
-        if(str.isEmpty()) return 0;
-        return 1+count(str.substring(1));
+    public static String scoreOfString(String s) {
+        int score = 0;
+        for (int i = 1; i < s.length(); i++) {
+            score += Math.abs(s.charAt(i) - s.charAt(i - 1));
+        }
+        // Convert score to a character based on its ASCII value
+        char resultChar = (char) score;
+        return Character.toString(resultChar);
     }
 
     public static void main(String[] args) {
-        String input="hello";
-        int length=count(input);
-        System.out.println(length);
+        String str = "zaz";
+        System.out.println(scoreOfString(str));
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
