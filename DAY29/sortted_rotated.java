0a81bdc03c8c07d09998ae525ff8d6e7b325432a
Vaibhav <itsvaibhav002@gmail.com>
2024-08-11 20:14:45 +0530

    update

--- DAY29/sortted_rotated.java
+++ DAY29/sortted_rotated.java
@@ -1,3 +1,36 @@
 public class sortted_rotated {
-    
+    public static int search(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+
+            if(nums[mid]==target) return mid;
+
+            else if(nums[mid]>=nums[left]){
+                if(nums[left]<=target && target<=nums[mid]){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+            }
+            
+            else{
+                if(nums[mid]<=target && target<=nums[right]){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
+            }
+            
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[]={4,5,6,1,3};
+        int target=1;
+        System.out.println(search(arr, target));
+    }
 }
