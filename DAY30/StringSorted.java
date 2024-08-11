0af085de835eceab0cb2a0d9b770625ec5c7b21c
Vaibhav <itsvaibhav002@gmail.com>
2024-08-11 20:41:44 +0530

    update

---
+++ DAY30/StringSorted.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class StringSorted {
+
+    public static void main(String[] args) {
+        String arr[] = {"sun", "earth", "mars", "mercury"};
+        Arrays.sort(arr); // Sort the array in place
+        System.out.println(Arrays.toString(arr)); // Print the sorted array
+    }
+}
