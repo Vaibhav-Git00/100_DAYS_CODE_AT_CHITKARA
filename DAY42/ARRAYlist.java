df1d5cec5945071a12ee772fed72f3fe3f42d9c8
Vaibhav <itsvaibhav002@gmail.com>
2024-08-23 20:24:31 +0530

    update

--- DAY42/ARRAYlist.java
+++ DAY42/ARRAYlist.java
@@ -1,5 +1,12 @@
-
-
+import java.util.*;
 public class ARRAYlist {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>(); 
+        //if you adding the value in arraylist
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        System.out.println(list);
+    }
 }
