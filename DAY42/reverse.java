29f32a9a2a759cc04eb132ef73c042ff3665eb5c
Vaibhav <itsvaibhav002@gmail.com>
2024-08-23 20:45:00 +0530

    update

---
+++ DAY42/reverse.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class reverse {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+        
+        //reverse
+        for(int i=list.size()-1;i>=0;i--){
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+    }
+}
