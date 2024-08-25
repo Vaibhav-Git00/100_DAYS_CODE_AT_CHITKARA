ba0b769caa2329e531eef1eaa461dd2f96311c2a
Vaibhav <itsvaibhav002@gmail.com>
2024-08-25 20:13:16 +0530

    update

---
+++ DAY44/monotonic.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class monotonic {
+    public static boolean monocheck(ArrayList<Integer> list){
+        boolean inc=true;
+        boolean dec=true;
+        for(int i=0;i<list.size()-1;i++){
+            if(list.get(i)>list.get(i+1)){
+                inc=false;
+            }
+            if(list.get(i)<list.get(i+1)){
+                dec=false;
+            }
+        }
+        return (inc||dec);
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+        System.out.println(monocheck(list));
+    }
+}
