64aa3d5569e3d77b522757fbb97e6c70f0bdb6e5
Vaibhav <itsvaibhav002@gmail.com>
2024-08-28 22:05:00 +0530

    update

---
+++ DAY47/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+    public static long countVowels(String word) {
+        long n = word.length();
+        long sum = 0;
+        List<Character> vowel = new ArrayList<>();
+        vowel.add('a');
+        vowel.add('e');
+        vowel.add('i');
+        vowel.add('o');
+        vowel.add('u');
+
+        for (int i = 0; i < n; i++) {
+            if (vowel.contains(word.charAt(i))) {
+                sum += (i + 1) * (n - i);
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        String s = "aba"; // Correct string initialization
+        long result = countVowels(s); // Call the countVowels method directly without an instance
+        System.out.println(result); // Print the result
+    }
+}
