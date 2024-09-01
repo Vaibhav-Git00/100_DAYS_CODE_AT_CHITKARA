124fb0b606982558d53cbea2c47da1c9460cd1c1
Vaibhav <itsvaibhav002@gmail.com>
2024-09-01 20:13:34 +0530

    update

---
+++ DAY51/linklist.java
@@ -0,0 +1,12 @@
+public class linklist {
+    public static class Node {   //this is the informataion of node class
+        int data;
+        Node next;    //this is next refrence of the next node
+    }
+    //constructor
+    public static Node head;
+    public static Node tail;
+    public static void main(String[] args) {
+        
+    }
+}
